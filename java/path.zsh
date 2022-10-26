d586b5796e848e15b7327ff0843bcdfef692aa3b
Ollie Bourne <obourne@spotify.com>
2022-10-26 09:48:01 +0100

    Set JAVA_HOME to sdkman current

--- java/path.zsh
+++ java/path.zsh
@@ -1,4 +1,4 @@
 
-#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="$HOME/.sdkman"
+export JAVA_HOME="$SDKMAN_DIR/candidates/java/current"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
