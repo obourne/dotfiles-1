bef797b2759d7b05c2cbc0976bc67f8bb1f92ccc
Ollie Bourne <obourne@spotify.com>
2023-06-28 10:54:47 +0100

    tweak

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,9 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="./bin:$HOME/.bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+# liquibase
+export PATH="/usr/local/opt/liquibase:$PATH"
+# postgres
+export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
+# flutter
+export PATH="$PATH:`pwd`/flutter/bin"
